b947805fc926c61fb882456c1ee660bf8a3fd8ca
Aryan Adhikari <aryanadhikari9790@gmail.com>
2026-02-11 14:47:00 +0545

    Create transport models for buses, routes, and stations

--- backend/transport/models.py
+++ backend/transport/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+class Bus:
+    def __init__(self, bus_id, capacity, route):
+        self.bus_id = bus_id
+        self.capacity = capacity
+        self.route = route
 
-# Create your models here.
+class Route:
+    def __init__(self, route_id, start_station, end_station, distance):
+        self.route_id = route_id
+        self.start_station = start_station
+        self.end_station = end_station
+        self.distance = distance
+
+class Station:
+    def __init__(self, station_id, name, location):
+        self.station_id = station_id
+        self.name = name
+        self.location = location
+
